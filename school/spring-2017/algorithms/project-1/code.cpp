e5f4733e2d28dd8014f02b72feabc582b48c8377
Mary Forde - Desktop <forde.ma@husky.neu.edu>
2017-01-25 10:31:15 -0500

    stuff from yesterday

--- school/spring-2017/algorithms/project-1/code.cpp
+++ school/spring-2017/algorithms/project-1/code.cpp
@@ -16,21 +16,54 @@ code::code(int n, int m) {
 	sequence.resize(n);
 	// initializes each of the values randomly, using n and m
 	randomCode(n, m);
-};
-
-int code::checkCorrect() {
-
 }
 
-int code::checkIncorrect(code c) {
+//constructor for the "code" class object
+// n is the length of the guess code, then the user inputs the values of the sequence
+code::code(int n) {
+	// asks the user for input, 
+	getGuessCode(n);
+}
 
+
+void code::getGuessCode(int n) {
+	sequence.clear();
+	
+	cout << "Enter your guess code, 1 value at a time." << endl;
+	
+	for(int i = 0; i < n; i++) {
+		int val;
+		cin >> val; //ADD INPUT CHECKING
+		sequence.push_back(val);
+	}
+}
+
+// Returns the number of digits which are the correct value and in the correct place
+int code::checkCorrect(const code &c) {
+	int numCorrect = 0;
+	for(int i = 0; i < sequence.size(); i++) {
+		if(getIndex(i) == c.getIndex(i)) {
+			numCorrect++;
+		}
+	}
+	
+	return numCorrect;
+}
+
+// Returns the number of digits which are the correct value and in the incorrect place
+int code::checkIncorrect(const code &c) {
+	return 0;
+}
+
+int code::getIndex(int i) const {
+	return sequence.at(i);
 }
 
 void code::randomCode(int n, int m) {
 	for (int i = 0; i < n; i++) {
 		sequence[i] = rand() % m;
 	}
-};
+}
 
 //prints the sequence vector
 void code::printCode() {
--- school/spring-2017/algorithms/project-1/main.cpp
+++ school/spring-2017/algorithms/project-1/main.cpp
@@ -1,13 +1,21 @@
 #include"code.h"
 #include<iostream>
 #include<vector>
+#include"time.h"
+#include"stdlib.h"
 
 using namespace std;
 
 int main() {
-	code testCode(5, 9);
-
-	testCode.printCode();
-
+	//initialize the seed for the random number generator
+	srand(time(NULL));
+	
+	code secretCode(5, 9);
+	secretCode.printCode();
+	code guessCode(5);
+	guessCode.printCode();
+	
+	cout << "NumCorrect: " << secretCode.checkCorrect(guessCode) << endl;
+	
 	return 0;
 }
